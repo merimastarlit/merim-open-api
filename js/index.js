8aec8efe0da9c66aa10ccf396d40c402a5e42b40
“MEERIM <meerim.us11@gmail.com>
2025-10-14 19:11:21 -0400

    fetched API, but no styling yet

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const apiUrl = "https://www.swapi.tech/api";
+const apiUrl = "https://www.swapi.tech/api/people/";
 
-fetch(`${apiUrl}/people/1`)
+fetch(apiUrl)
     .then((response) => {
 
         if (!response.ok) {
@@ -9,9 +9,48 @@ fetch(`${apiUrl}/people/1`)
         return response.json() // Parse JSON data
     })
     .then((data) => {
-        console.log("Character Data:", data);
-        return fetch(`${apiUrl}/planets/1`);
+        const people = document.getElementById("people");
+        const person = data.results;
+        person.forEach((person) => {
+            const personDiv = document.createElement("div");
+            personDiv.innerHTML = `
+                <h2>${person.name}</h2>
+            `;
+            people.appendChild(personDiv);
+        });
     })
     .catch((error) => {
         console.error("Error fetching data:", error);
     });
+
+
+
+
+//Starships: 
+
+const starshipsUrl = "https://www.swapi.tech/api/starships/";
+
+fetch(starshipsUrl)
+    .then((response) => {
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json() // Parse JSON data
+    })
+    .then((data) => {
+        const starships = document.getElementById("starships");
+        const starship = data.results;
+        starship.forEach((starship) => {
+            const starshipDiv = document.createElement("div");
+            starshipDiv.innerHTML = `
+                <h2>${starship.name}</h2>
+            `;
+            starships.appendChild(starshipDiv);
+        });
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+    });
+
+
